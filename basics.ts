d7da667d54cfb4e479f827f9aa1f3db903f34eed
mohammedyousuf <mohammedyousuf051@gmail.com>
2022-06-11 22:57:54 +0800

    Adding Objects

---
+++ basics.ts
@@ -0,0 +1,17 @@
+
+
+function add(num1: number, num2:number, isNumber: boolean){
+    if (isNumber == true){
+    console.log(num1 + num2);
+    }
+    else{
+        console.log("type is not a number");  
+    }
+
+}
+
+const n1 = 4;
+const n2 = 5.6;
+const isNum = true;
+
+const showAdd = add(n1, n2, isNum);
