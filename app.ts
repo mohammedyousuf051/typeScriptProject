45250ecc012ef937319adbe91391d80acf5035c5
mohammedyousuf <mohammedyousuf051@gmail.com>
2022-06-11 22:57:54 +0800

    Adding Objects

--- app.ts
+++ app.ts
@@ -1,17 +1,6 @@
-
-
-function add(num1: number, num2:number, isNumber: boolean){
-    if (isNumber == true){
-    console.log(num1 + num2);
-    }
-    else{
-        console.log("type is not a number");  
-    }
-
+const person = {
+    'name':'tommy',
+    'age':23
 }
 
-const n1 = 4
-const n2 = 5.6
-const isNum = true
-
-add(n1, n2, isNum);
+console.log(person);
